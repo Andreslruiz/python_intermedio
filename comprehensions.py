af4290b9151f221ca5d6313e21eded76ed0301f6
andreslruiz <ing.andreslruiz@gmail.com>
2022-01-09 13:14:29 -0500

    exceptions

--- comprehensions.py
+++ comprehensions.py
@@ -1,9 +1,9 @@
 def run():
-    # list={}
-    # for i in range(1,101):
-    #     if i%3!=0:
-    #         list[i]=i**3
-    # print(list)
+    list={}
+    for i in range(1,101):
+        if i%3!=0:
+            list[i]=i**3
+    print(list)
 
     dict={i: pow(i,2) for i in range(1,1001)}
     print(dict)
